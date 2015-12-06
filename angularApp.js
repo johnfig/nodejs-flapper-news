1013251c22f83f9733e3705ea4897c604bd09496
John Figueiredo <john@johnfigueiredo.com>
2015-12-06 08:30:06 -0800

    Add routing layer to angular

--- angularApp.js
+++ angularApp.js
@@ -7,6 +7,21 @@ app.factory('posts', [function(){
   return o;
 }]);
 
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
 app.controller('MainCtrl', [
 '$scope',
 'posts',
